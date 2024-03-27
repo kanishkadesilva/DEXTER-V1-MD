a69eb6c6ead31fe91e3497107b58a2c09c00d6a3
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-03-27 23:05:02 +0530

    Update ping.js

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('📌 *DEXTER-MD PING*\n ```' + 2000 + '``` *ms*') 
+    return repondre('📌 *Sahan-MD PING*\n ```' + 2000 + '``` *ms*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
