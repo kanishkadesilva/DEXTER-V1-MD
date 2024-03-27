6205501667e76898eed84c44cc2a93e8846e43aa
Sahan Maduwantha <125999503+maduwa2006@users.noreply.github.com>
2024-03-27 22:59:35 +0530

    Update Mods.js

--- commandes/Mods.js
+++ commandes/Mods.js
@@ -70,7 +70,7 @@ const sleep =  (ms) =>{
   
           const sticker = new Sticker(buffer.data, {
             pack: nomAuteurMessage,
-            author: "DEXTER-MD",
+            author: "Sahan-MD",
             type: StickerTypes.FULL,
             categories: ['🤩', '🎉'],
             id: '12345',
@@ -487,7 +487,7 @@ zokou({ nomCom: "save", categorie: "Mods" }, async (dest, zk, commandeOptions) =
           let media  = await zk.downloadAndSaveMediaMessage(msgRepondu.stickerMessage)
   
           let stickerMess = new Sticker(media, {
-            pack: 'DEXTER-MD',
+            pack: 'Sahan-MD',
             type: StickerTypes.CROPPED,
             categories: ["🤩", "🎉"],
             id: "12345",
@@ -539,7 +539,7 @@ zokou({
 
   if (alldata.length === 0 ) { repondre(`To activate or modify the mention; follow this syntax: mention link type message
   The different types are audio, video, image, and sticker.
-  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Dexter`) ; return}
+  Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Sahan`) ; return}
 
       if(data.status == 'non') {
           etat = 'Desactived'
@@ -560,7 +560,7 @@ Link: ${url}
 
 To activate or modify the mention, follow this syntax: mention link type message
 The different types are audio, video, image, and sticker.
-Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Dexter 
+Example: mention https://static.animecorner.me/2023/08/op2.jpg image Hi, my name is Sahan 
 
 To stop the mention, use mention stop`;
 
